583f00518e26ce405ca5e9a87e672bc82eb92188
yash <bhaliyayash595@gmail.com>
2026-02-18 17:15:35 +0530

    Fix API URL to use Vercel backend

--- public/config.js
+++ public/config.js
@@ -1,6 +1,5 @@
 // API Configuration
-const API_URL = window.location.hostname === 'localhost' 
-    ? 'http://localhost:5000' 
-    : window.location.origin;
+const API_URL = window.location.origin;
 
 console.log('API URL:', API_URL);
+console.log('Hostname:', window.location.hostname);
